3b6a8340fd3cda50c178d126d555373e8857e25b
Isuru Eranga <isurueranga.0@gmail.com>
2023-12-25 22:17:28 +0530

    footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -4,17 +4,17 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className=' w-full   bg-black flex flex-col items-center justify-center'>
+    <div className=' w-full bg-black flex flex-col items-center justify-center '>
             
-        <div className=' lg:flex lg:py-10 lg:gap-36'>
+        <div className=' lg:flex lg:py-10 lg:gap-36 flex-row space-y-10'>
 
             <div className='flex items-center justify-center'>
                 <Image src='Logo.svg' width={250} height={250} alt='home'/>
             </div>
 
-            <div className=' flex-col space-y-4'>
+            <div className=' flex-col space-y-4 flex items-center justify-center lg:justify-start lg:items-start'>
                 <p className=' text-white font-medium text-lg'>About</p>
-                <p className=' lg:w-[370px] text-gray-300 leading-8'>Hello Green" is an innovative web platform dedicated to addressing the pressing issue of non-biodegradable waste management.</p>
+                <p className=' lg:w-[370px] text-gray-300 leading-8 w-[300px] text-center lg:text-left'>Hello Green" is an innovative web platform dedicated to addressing the pressing issue of non-biodegradable waste management.</p>
 
                 <div className=' flex gap-3'>
                     <Link href=''><Image src='/assets/icons/Twitter.svg' width={60} height={60} alt='twitter'/></Link>
@@ -24,10 +24,10 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className=' flex-col space-y-4'>
+            <div className=' flex-col space-y-4 flex items-center justify-center lg:justify-start lg:items-start'>
                 <p className=' text-white font-medium text-lg'>Links</p>
 
-                <div className=' flex-row space-y-6'>
+                <div className='  space-y-3 flex-col flex items-center justify-center lg:items-start lg:justify-start'>
                     <Link href=''><p className='text-gray-300'>Request Pickup</p></Link>
                     <Link href=''><p className='text-gray-300'>Request Buy</p></Link>
                     <Link href=''><p className='text-gray-300'>About Usp</p></Link>
@@ -36,10 +36,10 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className=' flex-col space-y-4'>
+            <div className=' flex-col space-y-4 flex items-center justify-center lg:justify-start lg:items-start'>
                 <p className=' text-white font-medium text-lg'>Our Services</p>
 
-                <div className=' flex-row space-y-6'>
+                <div className=' space-y-3 flex-col flex items-center justify-center lg:items-start lg:justify-start'>
                     <Link href=''><p className='text-gray-300'>Waste Pickups</p></Link>
                     <Link href=''><p className='text-gray-300'>Waste Solutions</p></Link>
                     <Link href=''><p className='text-gray-300'>Roll-off Services</p></Link>
@@ -48,13 +48,13 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className=' flex-col space-y-4'>
+            <div className=' flex-col space-y-4 flex items-center justify-center lg:justify-start lg:items-start'>
                 <p className=' text-white font-medium text-lg'>Contact Us</p>
 
-                <div className=' flex-row space-y-3'>
-                    <Link href='' className=' flex gap-3'><Image src='/assets/icons/Phone.svg' width={20} height={20} alt='phone'/><p className='text-gray-300'>+94 76 260 5 123</p></Link>
-                    <Link href='' className=' flex gap-3'><Image src='/assets/icons/email.svg' width={20} height={20} alt='phone'/><p className='text-gray-300'>hellogreen@hello.com</p></Link>
-                    <Link href='' className=' flex gap-3'><Image src='/assets/icons/location.svg' width={20} height={20} alt='phone'/><p className='text-gray-300'>No: 91, Berker Street, London</p></Link>
+                <div className='  space-y-3 flex-col flex items-center justify-center lg:items-start lg:justify-start'>
+                    <Link href='' className=' items-center justify-center flex-col flex lg:flex-row gap-3'><Image src='/assets/icons/Phone.svg' width={20} height={20} alt='phone'/><p className='text-gray-300'>+94 76 260 5 123</p></Link>
+                    <Link href='' className=' items-center justify-center flex-col flex lg:flex-row gap-3'><Image src='/assets/icons/email.svg' width={20} height={20} alt='phone'/><p className='text-gray-300'>hellogreen@hello.com</p></Link>
+                    <Link href='' className=' items-center justify-center flex-col flex lg:flex-row gap-3'><Image src='/assets/icons/location.svg' width={20} height={20} alt='phone'/><p className='text-gray-300'>No: 91, Berker Street, London</p></Link>
                     
                 </div>
             </div>
@@ -65,7 +65,7 @@ const Footer = () => {
 
         <div>
 
-            <p>Copyright © 2022 Hello Green All Rights Reserved.</p>
+            <p className=' pt-5 text-white text-sm'>Copyright © 2022 Hello Green All Rights Reserved.</p>
 
         </div>
 
