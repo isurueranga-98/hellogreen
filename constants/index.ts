3b4caabf14c48a94f14ff7e3d7d3231ebd2d1d93
Isuru Eranga <isurueranga.0@gmail.com>
2023-12-31 08:23:45 +0530

    vercel json add

--- constants/index.ts
+++ constants/index.ts
@@ -1,60 +1,63 @@
 import { SidebarLink } from "@/types";
 
 export const sidebarLinks: SidebarLink[] = [
-    {
-      imgURL: "/assets/icons/home.svg",
-      route: "/",
-      label: "Home",
-    },
-    {
-      imgURL: "/assets/icons/users.svg",
-      route: "/articels",
-      label: "Articles",
-    },
-    // {
-    //   imgURL: "/assets/icons/star.svg",
-    //   route: "/about",
-    //   label: "About Us",
-    // },
-    // {
-    //   imgURL: "/assets/icons/suitcase.svg",
-    //   route: "/contactus",
-    //   label: "Contact Us",
-    // },
-    
-  ];
+  {
+    imgURL: "/assets/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/icons/users.svg",
+    route: "/articels",
+    label: "Articles",
+  },
+  {
+    imgURL: "/assets/icons/star.svg",
+    route: "/about",
+    label: "About Us",
+  },
+  {
+    imgURL: "/assets/icons/suitcase.svg",
+    route: "/contactus",
+    label: "Contact Us",
+  },
+];
 
 // constants/index.ts
 export const articles = [
   {
     id: 1,
-    img: '/assets/images/HeroIMG.png',
+    img: "/assets/images/HeroIMG.png",
     title: "Recurring Waste Services",
-    description: "Wastey is a single-bin recycling service that returns more of your discarded items to the environment .Wastey is a single-bin recycling service that returns more of your discarded items to the environment",
+    description:
+      "Wastey is a single-bin recycling service that returns more of your discarded items to the environment .Wastey is a single-bin recycling service that returns more of your discarded items to the environment",
   },
   {
     id: 2,
-    img: '/assets/images/HeroIMG.png',
+    img: "/assets/images/HeroIMG.png",
     title: "Recurring Waste Services",
-    description: "Wastey is a single-bin recycling service that returns more of your discarded items to the environment. Wastey is a single-bin recycling service that returns more of your discarded items to the environment",
+    description:
+      "Wastey is a single-bin recycling service that returns more of your discarded items to the environment. Wastey is a single-bin recycling service that returns more of your discarded items to the environment",
   },
   {
     id: 2,
-    img: '/assets/images/HeroIMG.png',
+    img: "/assets/images/HeroIMG.png",
     title: "Recurring Waste Services",
-    description: "Wastey is a single-bin recycling service that returns more of your discarded items to the environment. Wastey is a single-bin recycling service that returns more of your discarded items to the environment",
+    description:
+      "Wastey is a single-bin recycling service that returns more of your discarded items to the environment. Wastey is a single-bin recycling service that returns more of your discarded items to the environment",
   },
   {
     id: 2,
-    img: '/assets/images/HeroIMG.png',
+    img: "/assets/images/HeroIMG.png",
     title: "Recurring Waste Services",
-    description: "Wastey is a single-bin recycling service that returns more of your discarded items to the environment. Wastey is a single-bin recycling service that returns more of your discarded items to the environment",
+    description:
+      "Wastey is a single-bin recycling service that returns more of your discarded items to the environment. Wastey is a single-bin recycling service that returns more of your discarded items to the environment",
   },
   {
     id: 2,
-    img: '/assets/images/HeroIMG.png',
+    img: "/assets/images/HeroIMG.png",
     title: "Recurring Waste Services",
-    description: "Wastey is a single-bin recycling service that returns more of your discarded items to the environment. Wastey is a single-bin recycling service that returns more of your discarded items to the environment",
+    description:
+      "Wastey is a single-bin recycling service that returns more of your discarded items to the environment. Wastey is a single-bin recycling service that returns more of your discarded items to the environment",
   },
-  
 ];
